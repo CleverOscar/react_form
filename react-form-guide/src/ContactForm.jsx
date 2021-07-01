79cfdda951d5da4e313b885656dbf9bd3b3d959d
Oscar Pacheco <Oscarpa9188@gmail.com>
2021-07-01 17:56:46 -0500

    Basic Form Built

--- react-form-guide/src/ContactForm.jsx
+++ react-form-guide/src/ContactForm.jsx
@@ -13,7 +13,28 @@ class ContactForm extends Component {
     render(){
         return(
             <div>
-                Contact Form 
+                <Form id="contact-form">
+                    <FormGroup row>
+                        <Label for="name" sm={2}>Email</Label>
+                        <Col sm={10}>
+                        <Input className="inputStyle" type="text" name="name" id="name" placeholder="Full" />
+                        </Col>
+                    </FormGroup>
+                    
+                    <FormGroup row>
+                        <Label for="exampleEmail" sm={2}>Email</Label>
+                        <Col sm={10}>
+                        <Input className="inputStyle" type="email" name="email" id="exampleEmail" placeholder="Email Address" />
+                        </Col>
+                    </FormGroup>
+
+                    <FormGroup row>
+                        <Label for="emailTextArea" sm={2}>Text Area</Label>
+                        <Col sm={10}>
+                        <Input className="textStyle" type="textarea" name="emialTextArea" id="textArea" />
+                        </Col>
+                    </FormGroup>
+                </Form>
             </div>
         )
     }
